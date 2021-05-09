860329b9e86997e9e312067f375e4c3129ff77d5
alfend <alfend@yandex.ru>
2021-05-09 09:22:06 +0500

    les2

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,37 @@
 const path = require("path");
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-   entry: {
-       app: './index.js',
-   },
-   context: path.resolve(__dirname, "static_src"),
-   output: {
-       path: path.resolve(__dirname, "static", "build"),
-       filename: 'app.js',
-   },
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /.(css|scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 };
